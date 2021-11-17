01d1254d5e65d277de9b41c103024d7b8eaed9bb
juliopdx <juliop503@gmail.com>
2021-11-17 12:46:38 -0800

    Tags working and removed some app calls due to no licensing

--- connect.py
+++ connect.py
@@ -5,12 +5,13 @@ from panos import objects
 from panos import network
 from panos import device
 from rich import print, inspect
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 
 fw = firewall.Firewall("192.168.10.192", "admin", "PaloAlto123!")
 
 
-# Creating Zones on firewall
+# Creating Zones on Firewall
 lan_zone = network.Zone(
     name="User LAN",
     mode="layer3",
@@ -18,7 +19,6 @@ lan_zone = network.Zone(
     log_setting="Zone-Forwarding-Profile",
     enable_packet_buffer_protection=True,
 )
-
 fw.add(lan_zone)
 lan_zone.create()
 
@@ -30,7 +30,6 @@ dc_zone = network.Zone(
     log_setting="Zone-Forwarding-Profile",
     enable_packet_buffer_protection=True,
 )
-
 fw.add(dc_zone)
 dc_zone.create()
 
@@ -42,7 +41,6 @@ dmz_zone = network.Zone(
     log_setting="Zone-Forwarding-Profile",
     enable_packet_buffer_protection=True,
 )
-
 fw.add(dmz_zone)
 dmz_zone.create()
 
@@ -54,12 +52,45 @@ outside_zone = network.Zone(
     log_setting="Zone-Forwarding-Profile",
     enable_packet_buffer_protection=True,
 )
-
 fw.add(outside_zone)
 outside_zone.create()
 
 
-# Configuring main firewall interfaces
+# Custom Tags
+tags = {
+    "any": {
+        "color": "brown",
+    },
+    "User LAN": {
+        "color": "green",
+    },
+    "DMZ": {
+        "color": "yellow",
+    },
+    "Outside": {
+        "color": "red",
+    },
+    "Datacenter": {
+        "color": "blue gray",
+    },
+    "Internet Gateway": {
+        "color": "cyan",
+    },
+    "Temporary": {
+        "color": "black",
+    },
+    "IT": {
+        "color": "light green",
+    },
+}
+
+for k, v in tags.items():
+    current_tag = objects.Tag(name=k, color=objects.Tag.color_code(v["color"]))
+    fw.add(current_tag)
+    current_tag.create()
+
+
+# Configuring Main Firewall Interfaces
 outside_intf = network.EthernetInterface(
     name="ethernet1/1",
     mode="layer3",
@@ -125,12 +156,13 @@ fw.add(lan_intf)
 lan_intf.create()
 
 
-# Standing up Virtual Router
+# Virtual Router
 vr_fw = network.VirtualRouter(
     name="default",
 )
 fw.add(vr_fw)
 vr_fw.create()
+
 
 # Redistribution Profile
 redis = network.RedistributionProfile(
@@ -141,6 +173,7 @@ redis = network.RedistributionProfile(
 )
 vr_fw.add(redis)
 redis.create()
+
 
 # Configuring OSPF process
 ospf_fw = network.Ospf(
@@ -167,7 +200,17 @@ for prefix in area_0_ranges:
 
 area_0_interfaces = ["ethernet1/3", "ethernet1/4"]
 for interface in area_0_interfaces:
-    ospf_interface = network.OspfAreaInterface(name=interface, enable=True, link_type="broadcast")
+    ospf_interface = network.OspfAreaInterface(
+        name=interface, enable=True, link_type="broadcast", passive=False
+    )
+    ospf_area.add(ospf_interface)
+    ospf_interface.create()
+
+area_0_interfaces_passive = ["ethernet1/2"]
+for interface in area_0_interfaces_passive:
+    ospf_interface = network.OspfAreaInterface(
+        name=interface, enable=True, link_type="p2p", passive=True
+    )
     ospf_area.add(ospf_interface)
     ospf_interface.create()
 
@@ -178,6 +221,200 @@ ospf_export = network.OspfExportRules(
 ospf_fw.add(ospf_export)
 ospf_export.create()
 
-print("[bold yellow]Committing configuration...[/bold yellow]")
-print(fw.commit(sync=True))
-print("[bold green]Complete...[/bold green]")
+
+# Application Filters
+# evasive_filter = objects.ApplicationFilter(name="Evasive Applications", evasive=True)
+# fw.add(evasive_filter)
+# evasive_filter.create()
+
+# peer_to_peer_filter = objects.ApplicationFilter(
+#     name="peer-to-peer", technology=["peer-to-peer"]
+# )
+# fw.add(peer_to_peer_filter)
+# peer_to_peer_filter.create()
+
+# encrypted_tunnels_filter = objects.ApplicationFilter(
+#     name="encrypted-tunnels", subcategory=["encrypted-tunnel"]
+# )
+# fw.add(encrypted_tunnels_filter)
+# encrypted_tunnels_filter.create()
+
+# remote_access_filter = objects.ApplicationFilter(
+#     name="remote-access", subcategory=["remote-access"]
+# )
+# fw.add(remote_access_filter)
+# remote_access_filter.create()
+
+# general_business_filter = objects.ApplicationFilter(
+#     name="general-business",
+#     category="business-systems",
+#     subcategory=["general-business"],
+# )
+# fw.add(general_business_filter)
+# general_business_filter.create()
+
+
+# Application Groups
+# denied_apps_group = objects.ApplicationGroup(
+#     name="Denied Applications",
+#     value=[
+#         "Evasive Applications",
+#         "peer-to-peer",
+#         "encrypted-tunnels",
+#         "remote-access",
+#     ],
+# )
+# fw.add(denied_apps_group)
+# denied_apps_group.create()
+
+# sanctioned_saas_group = objects.ApplicationGroup(
+#     name="Sanctioned SaaS",
+#     value=["asana", "salesforce", "bloomberg-professional", "amazon-aws-console"],
+# )
+# fw.add(sanctioned_saas_group)
+# sanctioned_saas_group.create()
+
+
+# Custom App Example
+# globo_web_app = objects.ApplicationObject(
+#     name="Globo-Web-Front",
+#     description="Some Fake Application",
+#     category="business-systems",
+#     subcategory="general-business",
+#     technology="browser-based",
+#     risk=3,
+# )
+# fw.add(globo_web_app)
+# globo_web_app.create()
+
+
+# Custom Service Object
+# udp_80_service = objects.ServiceObject(
+#     name="UDP_80", description="QUIC Port", protocol="udp", destination_port="80"
+# )
+# fw.add(udp_80_service)
+# udp_80_service.create()
+
+# udp_443_service = objects.ServiceObject(
+#     name="UDP_443",
+#     description="QUIC Port",
+#     protocol="udp",
+#     destination_port="443",
+# )
+# fw.add(udp_443_service)
+# udp_443_service.create()
+
+
+# Custom Service Group
+# quic_service_group = objects.ServiceGroup(
+#     name="QUIC_Ports", value=["UDP_80", "UDP_443"]
+# )
+# fw.add(quic_service_group)
+# quic_service_group.create()
+
+
+# print("[bold yellow]Committing configuration...[/bold yellow]")
+# print(fw.commit(sync=True))
+# print("[bold green]Please see committ output above...[/bold green]")
+
+
+# Address objects
+address_objects = {
+    "Contractors": {
+        "address": "10.17.11.0/24",
+    },
+    "Datacenter Applications": {
+        "address": "10.172.22.0/24",
+    },
+    "Datacenter Infrastructure": {
+        "address": "10.172.21.0/24",
+    },
+    "Datacenter Internal DNS": {
+        "address": "10.172.21.5",
+    },
+    "OOBM": {
+        "address": "172.20.1.0/24",
+    },
+    "Operations-Finance": {
+        "address": "10.17.6.0/24",
+    },
+    "Operations-Sales": {
+        "address": "10.17.8.0/24",
+    },
+    "IT Administrators": {
+        "address": "10.17.7.0/24",
+    },
+    "IT Developers": {
+        "address": "10.17.9.0/24",
+    },
+}
+for k, v in address_objects.items():
+    current_address = objects.AddressObject(
+        name=k, value=v["address"], type="ip-netmask"
+    )
+    fw.add(current_address)
+    current_address.create()
+
+
+# Address Groups
+it_users_group = objects.AddressGroup(
+    name="IT Users", static_value=["IT Administrators", "IT Developers"], tag="User LAN"
+)
+fw.add(it_users_group)
+it_users_group.create()
+
+operations_user_group = objects.AddressGroup(
+    name="Operations User Subnets",
+    static_value=["Operations-Finance", "Operations-Sales"],
+    tag="User LAN",
+)
+fw.add(operations_user_group)
+operations_user_group.create()
+
+datacenter_user_group = objects.AddressGroup(
+    name="Datacenter Subnets",
+    static_value=[
+        "Datacenter Applications",
+        "Datacenter Infrastructure",
+        "Datacenter Internal DNS",
+    ],
+    tag="Datacenter",
+)
+fw.add(datacenter_user_group)
+datacenter_user_group.create()
+
+all_users_group = objects.AddressGroup(
+    name="All User Subnets",
+    static_value=["IT Users", "Operations User Subnets"],
+    tag="User LAN",
+)
+fw.add(all_users_group)
+all_users_group.create()
+
+
+# Security Profile Groups
+# Profiles must exist, did not find call to create
+# individual security profiles
+custom_spg = objects.SecurityProfileGroup(
+    name="Internet Security Profile",
+    virus="Strict_AV",
+    spyware="BP_Spyware",
+    vulnerability="BP_VPP",
+    url_filtering="BP_URLFiltering",
+    file_blocking="strict file blocking",
+    data_filtering=None,
+    wildfire_analysis="default",
+)
+fw.add(custom_spg)
+custom_spg.create()
+
+
+with Progress(
+    SpinnerColumn("aesthetic", speed=0.4), TextColumn("{task.description}")
+) as progress:
+
+    task1 = progress.add_task("[bold yellow]Committing Configuration...", total=1)
+    while not progress.finished:
+        print(fw.commit(sync=True))
+        progress.update(task1, advance=1)
+print("[bold green]Please see committ output above...[/bold green]")
