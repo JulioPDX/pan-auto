0fca00d2fc992cb92713a14f1d6feb1f395b9a20
juliopdx <juliop503@gmail.com>
2021-11-22 16:19:54 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pan-auto
-Repo for testing python interaction with a Palo Alto device. 
+# Configuring a PAN Firewall with the pan-os-python SDK
+
+Thank you for checking out this repository. What you see here is a collection of my learnings working through the PAN Python SDK and Craig Stansburys course on Pluralsight (great course). I hope this gives you an idea of what is possible and maybe start automating some firewall configurations.
+
+## Getting Started
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+git clone https://github.com/JulioPDX/pan-auto.git
+pip install -r requirements.txt
+```
+
+## Modify
+
+At this point feel free to modify the code or use the examples as reference to build out anything you want!
+
+## Blog
+
+If you have some spare time or just curious, feel free to read through the blog post referencing this repository.
+
+- [My Journey Learning about the Palo Alo Networks Python SDK](https://juliopdx.com/2021/11/22/my-journey-learning-about-the-palo-alto-networks-python-sdk/)
--- configs/zones.py
+++ configs/zones.py
@@ -2,36 +2,26 @@ zones = [
     {
         "name": "User LAN",
         "mode": "layer3",
-        "zone_profile": "DefaultZoneProtectionProfile",
-        "log_setting": "Zone-Forwarding-Profile",
         "enable_packet_buffer_protection": True,
     },
     {
         "name": "Datacenter",
         "mode": "layer3",
-        "zone_profile": "DefaultZoneProtectionProfile",
-        "log_setting": "Zone-Forwarding-Profile",
         "enable_packet_buffer_protection": True,
     },
     {
         "name": "DMZ",
         "mode": "layer3",
-        "zone_profile": "DefaultZoneProtectionProfile",
-        "log_setting": "Zone-Forwarding-Profile",
         "enable_packet_buffer_protection": True,
     },
     {
         "name": "Outside",
         "mode": "layer3",
-        "zone_profile": "OutsideZoneProtectionProfile",
-        "log_setting": "Zone-Forwarding-Profile",
         "enable_packet_buffer_protection": True,
     },
     {
         "name": "Test",
         "mode": "layer3",
-        "zone_profile": "OutsideZoneProtectionProfile",
-        "log_setting": "Zone-Forwarding-Profile",
         "enable_packet_buffer_protection": True,
     },
 ]
